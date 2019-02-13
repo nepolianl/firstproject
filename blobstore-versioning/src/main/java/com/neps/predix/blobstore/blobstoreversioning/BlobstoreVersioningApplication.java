f031735377414543bb2818d1af91c4cdc1847c4d
nepolianl <nepolian.l@tcs.com>
2019-02-13 23:17:58 +0530

    first commit
    first commit

---
+++ blobstore-versioning/src/main/java/com/neps/predix/blobstore/blobstoreversioning/BlobstoreVersioningApplication.java
@@ -0,0 +1,12 @@
+package com.neps.predix.blobstore.blobstoreversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlobstoreVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlobstoreVersioningApplication.class, args);
+	}
+}
---
+++ blobstore-versioning/src/test/java/com/neps/predix/blobstore/blobstoreversioning/BlobstoreVersioningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neps.predix.blobstore.blobstoreversioning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlobstoreVersioningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
